8c863ce281d74b0e67c88e873bb3b2a068904f6c
Abdulaziz Malikov <79694711+abdulazizmalikov18@users.noreply.github.com>
2025-01-16 15:45:22 +0500

    update

--- lib/assets/assets/icons.dart
+++ lib/assets/assets/icons.dart
@@ -6,6 +6,10 @@ class AppIcons {
   static const checkboxRadioA = "assets/icons/checkbox_radio_a.svg";
   static const checkboxRadioD = "assets/icons/checkbox_radio_d.svg";
   static const add = "assets/icons/add.svg";
+  static const noResult = "assets/icons/no_result.svg";
+  static const noConnection = "assets/icons/no_connection.svg";
+  static const emptyFile = "assets/icons/empty_file.svg";
+  static const logoCarting = "assets/icons/logo_carting.svg";
   static const calendar = "assets/icons/calendar.svg";
   static const editCir = "assets/icons/edit_cir.svg";
   static const cash = "assets/icons/cash.svg";
--- lib/presentation/views/splash_view.dart
+++ lib/presentation/views/splash_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-import 'package:carting/assets/assets/images.dart';
+import 'package:carting/assets/assets/icons.dart';
 import 'package:carting/assets/colors/colors.dart';
 
 class SplashView extends StatefulWidget {
@@ -16,57 +16,41 @@ class _SplashViewState extends State<SplashView> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      body: DecoratedBox(
-        decoration: BoxDecoration(
-          color: const Color(0xFF66C61C).withValues(alpha: .7),
-        ),
-        child: Stack(
-          alignment: Alignment.center,
-          fit: StackFit.expand,
-          children: [
-            Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 48),
-                    child: AppImages.logoText.imgAsset(),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 32, vertical: 24),
-                    child: Text(
-                      "O’zbekiston bo’ylab yuk tashish",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 120)
-                ],
-              ),
+      body: Stack(
+        alignment: Alignment.center,
+        fit: StackFit.expand,
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 48),
+                  child: AppIcons.logoCarting.svg(height: 24),
+                ),
+                const SizedBox(height: 100),
+              ],
             ),
-            const Positioned(
-              bottom: 56,
-              child: Column(
-                children: [
-                  SpinKitCircle(
-                    size: 52,
-                    color: Colors.white,
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    '© All rights reserved',
-                    style: TextStyle(color: white),
-                    textAlign: TextAlign.center,
-                  )
-                ],
-              ),
+          ),
+          Positioned(
+            bottom: 56,
+            child: Column(
+              children: [
+                const SpinKitCircle(
+                  size: 52,
+                  color: Color(0xFF030303),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  '© All rights reserved',
+                  style: TextStyle(color: dark.withValues(alpha: .3)),
+                  textAlign: TextAlign.center,
+                )
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
